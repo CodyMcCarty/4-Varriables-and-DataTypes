464ebf300863e6f591c46898e17e82533ff880ff
Cody McCarty <codyjmccarty@gmail.com>
2022-11-12 13:31:54 -0600

    hex bi ect

--- main.cpp
+++ main.cpp
@@ -1,6 +1,15 @@
 #include <iostream>
 
 int main(){
-    auto result = (10 <=> 20) > 0;
-    std::cout << result << std::endl;
+    int num1 = 15; // Decimal
+    int num2 = 017; // Oct
+    int num3 = 0x0f; // Hex
+    int num4 = 0b00001111; // Binary
+
+    std::cout << "num " << num1 << std::endl;
+    std::cout << "num " << num2 << std::endl;
+    std::cout << "num " << num3 << std::endl;
+    std::cout << "num " << num4 << std::endl;
+
+    return 0;
 }
