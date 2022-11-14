d4ba980808b8ecc63bf6101f6b7ad0d03978b6d6
Cody McCarty <codyjmccarty@gmail.com>
2022-11-14 12:27:25 -0600

    intModifiers

--- main.cpp
+++ main.cpp
@@ -1,15 +1,10 @@
 #include <iostream>
 
 int main(){
-    int num1 = 15; // Decimal
-    int num2 = 017; // Oct
-    int num3 = 0x0f; // Hex
-    int num4 = 0b00001111; // Binary
-
-    std::cout << "num " << num1 << std::endl;
-    std::cout << "num " << num2 << std::endl;
-    std::cout << "num " << num3 << std::endl;
-    std::cout << "num " << num4 << std::endl;
+    int num1{10};
+    int num2{-300};
+    unsigned int num3{40};
+    // unsigned int num4{-40}; // error: narrowing conversion
 
     return 0;
 }
