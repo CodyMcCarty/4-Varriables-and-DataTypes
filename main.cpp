2d2741518be65385c62173c9cdb1c7d1d5ae4853
Cody McCarty <codyjmccarty@gmail.com>
2022-11-16 11:28:41 -0600

    array

--- main.cpp
+++ main.cpp
@@ -6,5 +6,12 @@ int main(){
     unsigned int num3{40};
     // unsigned int num4{-40}; // error: narrowing conversion
 
+    int nums[]{2, 4, 6, 8};
+    for (auto num : nums){
+        std::cout << num << std::endl;
+    }
+
+    std::cout << nums << std::endl;
+
     return 0;
 }
